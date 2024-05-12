00058794c426114819cd14980f2a6fd24e92f1ea
kimpepe <unboxing96@gmail.com>
2024-05-12 11:03:33 +0900

    Initial Commit

---
+++ OliveYoungComp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OliveYoungComp
+//
+//  Created by 김태현 on 5/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OliveYoungCompUITests/OliveYoungCompUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  OliveYoungCompUITestsLaunchTests.swift
+//  OliveYoungCompUITests
+//
+//  Created by 김태현 on 5/12/24.
+//
+
+import XCTest
+
+final class OliveYoungCompUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
