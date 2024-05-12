95b7878da76924d743b41a835f8d9f5108609bee
kimpepe <unboxing96@gmail.com>
2024-05-12 11:45:00 +0900

    feat: WKWebView로 올리브영 몰 추가 및 동작 확인

--- OliveYoungComp/ViewController.swift
+++ OliveYoungComp/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.translatesAutoresizingMaskIntoConstraints = false // 오토레이아웃 제약을 직접 설정
+        view.addSubview(webView)
+        
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            webView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor)
+        ])
+
+        if let url = URL(string: "https://www.oliveyoung.co.kr/store/main/main.do?oy=0") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
-
-
 }
+
 
